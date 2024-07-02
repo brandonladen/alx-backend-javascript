02a219aa446b6b969ca19c3e4829c08c2da14fc3
Brandon Odhiambo Wamboga <95022425+brandonladen@users.noreply.github.com>
2024-07-02 05:55:27 +0300

    Update 4-update_grade_by_city.js

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,12 +1,12 @@
 export default function updateStudentGradeByCity(arrayObj, city, newGrades) {
   const cleanData = arrayObj.filter((obj) => obj.location === city);
   if (!Array.isArray(newGrades)) {
-    return cleanData.map((obj) => ({ ...obj, grade: 'NA' }));
+    return cleanData.map((obj) => ({ ...obj, grade: 'N/A' }));
   }
   return cleanData.map((obj) => {
     const grade = newGrades.find((g) => g.studentId === obj.id);
     return {
-      ...obj, grade: grade ? grade.grade : 'NA',
+      ...obj, grade: grade ? grade.grade : 'N/A',
     };
   });
 }
