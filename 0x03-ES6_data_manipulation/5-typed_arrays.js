ad6e9752ce98a8ecc35c2995573e0e3d8e9de0b7
brandonladen <brandonladen486@gmail.com>
2024-07-02 03:15:33 +0000

    fixed lint error

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -3,7 +3,7 @@ export default function createInt8TypedArray(length, position, value) {
     throw new Error('Position outside range');
   }
   const buffer = new ArrayBuffer(length);
-	const dataView = new DataView(buffer);
-	dataView.setInt8(position, value);
-	return dataView;
+  const dataView = new DataView(buffer);
+  dataView.setInt8(position, value);
+  return dataView;
 }
